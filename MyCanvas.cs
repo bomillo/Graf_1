3a56b63284d80f3533d12068cb4ef9f69a3d370d
Dominik Postolowicz <dominik.pos@wp.pl>
2023-10-13 23:32:35 +0200

    graf 2

--- MyCanvas.cs
+++ MyCanvas.cs
@@ -2,22 +2,15 @@
 {
     internal class MyCanvas : IDrawable
     {
-        public List<Shape> shapes;
+        public Shape shapes;
 
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
             canvas.StrokeSize = 0;
             canvas.FillColor = Colors.White;
             canvas.FillRectangle(dirtyRect);
-            /*
-            canvas.FillColor = Colors.Red;
-            canvas.FillRectangle(0, 0, 5, 5);
-            canvas.FillRectangle(100, 0, 5, 5);
-            canvas.FillRectangle(0, 100, 5, 5);
-            canvas.FillRectangle(100, 100, 5, 5);
-            */
 
-            shapes?.ForEach(s => s.DrawOnCanvas(canvas));
+            shapes?.DrawOnCanvas(canvas);
         }
 
     }
